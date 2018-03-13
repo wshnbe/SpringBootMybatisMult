ac2055a26fc5bb508c4be248456367c44f6a3199
wll <wshnbe@sina.com>
2018-03-13 15:41:19 +0800

    使用springBoot2.0

--- src/main/java/com/basjoo/SpringBootMybatisMult/datasource/DataSourceConfig.java
+++ src/main/java/com/basjoo/SpringBootMybatisMult/datasource/DataSourceConfig.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 import javax.sql.DataSource;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
